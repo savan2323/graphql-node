4c3b8784a07e5713821e282b87c616cbb1d36615
savan2323 <savanrajkotiya@gmail.com>
2019-11-17 17:42:48 +0530

    initial setup1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const typeDefs = gql`
     author: String
   }
 
-  # The "Query" type is special: it lists all of the available queries that
+  # The "Query" typeis special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
